9958121d5fa101de0c1ab591c5390bc24d5770bc
JoeMadejski <67700470+JoeMadejski@users.noreply.github.com>
2020-07-01 17:10:58 -0400

    Add files via upload

---
+++ inc/nav.php
@@ -0,0 +1,10 @@
+<nav class="menu">
+	<ul>
+		<li> <a href="index.php">Home</a></li>
+		<li> <a href=background.php>Background</a></li>
+		<li> <a href=claws.php>Claws</a></li>
+		<li> <a href=culinaryuses.php>Culinary Uses</a></li>
+		<li> <a href=coolfeatures.php>Cool Features</a></li>
+		<li> <a href="contact.php">Contact Us!</a></li>
+	</ul>
+</nav>
